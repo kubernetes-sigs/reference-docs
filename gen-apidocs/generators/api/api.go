e47dd8952e47b493d108204fc703166f629eeaaa
Qiming Teng <tengqim@cn.ibm.com>
2018-07-16 15:10:24 +0000

    Rework API reference generator
    This PR reworks the generator for API references:
    
    - Generalize the writer and reorganize packages and modules;
    - Add new generator for producing HTML result directly;
    - Fixed swagger definition that missed SchedulingClass definition.
    
    With this revised version:
    
    - We fix the problem of example code display which no longer occupy 45%
      of the screen;
    - The dependency on node JS;
    - The improper use of Go Template is gone;
    - The dependency on a docker image which team doesn't own is gone.
    
    The current "brodocs" way of reference generation is still there. We
    can remove it later when we are satisfied with this new "native"
    generator.
    
    To use the old "brodocs" generator, do `make brodocsapi` instead of
    `make api`.

--- gen-apidocs/generators/api/api.go
+++ gen-apidocs/generators/api/api.go
@@ -16,11 +16,10 @@ limitations under the License.
 
 package api
 
-import "strings"
-
-type ApiGroup string
-
-type ApiGroups []ApiGroup
+import (
+	"regexp"
+	"strings"
+)
 
 func (a ApiGroup) String() string {
 	return string(a)
@@ -39,10 +38,49 @@ func (a ApiGroups) Less(i, j int) bool {
 	return strings.Compare(a[i].String(), a[j].String()) < 0
 }
 
-type ApiVersions []ApiVersion
-
 func (a ApiVersions) Len() int      { return len(a) }
 func (a ApiVersions) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a ApiVersions) Less(i, j int) bool {
 	return a[i].LessThan(a[j])
+}
+
+func (k ApiKind) String() string {
+	return string(k)
+}
+
+func (this ApiVersion) LessThan(that ApiVersion) bool {
+	re := regexp.MustCompile("(v\\d+)(alpha|beta|)(\\d*)")
+	thisMatches := re.FindStringSubmatch(string(this))
+	thatMatches := re.FindStringSubmatch(string(that))
+
+	a := []string{thisMatches[1]}
+	if len(thisMatches) > 2 {
+		a = []string{thisMatches[2], thisMatches[1], thisMatches[0]}
+	}
+
+	b := []string{thatMatches[1]}
+	if len(thatMatches) > 2 {
+		b = []string{thatMatches[2], thatMatches[1], thatMatches[0]}
+	}
+
+	for i := 0; i < len(a) && i < len(b); i++ {
+		v1 := ""
+		v2 := ""
+		if i < len(a) {
+			v1 = a[i]
+		}
+		if i < len(b) {
+			v2 = b[i]
+		}
+		// If the "beta" or "alpha" is missing, then it is ga (empty string comes before non-empty string)
+		if len(v1) == 0 || len(v2) == 0 {
+			return v1 < v2
+		}
+		// The string with the higher number comes first (or in the case of alpha/beta, beta comes first)
+		if v1 != v2 {
+			return v1 > v2
+		}
+	}
+
+	return false
 }
