2e0428c96ab345e46faede6abb70b4882190c560
Varsha U N <varshaun58@gmail.com>
2026-03-26 10:32:39 +0530

    fix: handle abbreviations like X.509 in getEscapedFirstPhrase

--- gen-resourcesdocs/pkg/config/helpers.go
+++ gen-resourcesdocs/pkg/config/helpers.go
@@ -1,8 +1,22 @@
 package config
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
+
+// sentenceEnd matches a period that is likely a real sentence boundary:
+// it must be followed by a space (or end of string) and NOT preceded by
+// a single uppercase letter (which would indicate an abbreviation like X.509)
+var sentenceEnd = regexp.MustCompile(`(?:[^A-Z])\.\s`)
 
 func getEscapedFirstPhrase(s string) string {
-	description := strings.Split(s, ".")[0] + "."
+	loc := sentenceEnd.FindStringIndex(s)
+	var description string
+	if loc != nil {
+		description = s[:loc[0]+2]
+	} else {
+		description = s
+	}
 	return strings.ReplaceAll(description, "\"", "\\\"")
 }
